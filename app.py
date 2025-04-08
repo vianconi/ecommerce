c40706e713d940dec5693cabdeece7f88aed8394
user <emiliovianconi@gmail.com>
2025-04-08 17:28:57 -0300

    my second commit

--- app.py
+++ app.py
@@ -1,22 +1,90 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
 db = SQLAlchemy(app)
 
-class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
 
-with app.app_context():
-    db.create_all()
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+@app.route('/api/products/add', methods=['POST'])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data.get('name'), price=data.get('price'), description=data.get('description', ''))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({'message': 'Product added successfully'}), 200
+    return jsonify({'message': 'Invalid product data'}), 400
+
+@app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({'message': 'Product deleted successfully'}), 200
+    return jsonify({'message': 'Product not found'}), 404
+
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'description': product.description
+        }), 200
+    return jsonify({'message': 'Product not found'}), 404
+
+@app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({'message': 'Product not found'}), 404
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+
+    if 'price' in data:
+        product.price = data['price']
+
+    if 'description' in data:
+        product.description = data['description']
+
+    db.session.commit()
+    return jsonify({'message': 'Product updated successfully'})
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'description': product.description
+        }
+        product_list.append(product_data)
+    return jsonify(product_list)
 
 @app.route('/')
 def index():
-    return '¡Conectando a PostgreSQL!'
+    return 'index'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:Password2025@localhost:5432/ecommerce")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
